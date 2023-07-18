acf392cae2cb0cdb517d4912dbed7a7549f2e7d8
Jordan Kay <jordan@fleuronic.com>
2023-07-18 16:54:33 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
 	name: "Catenoid",
 	platforms: [
-		.iOS(.v12),
-		.macOS(.v10_13),
-		.watchOS(.v6),
-		.tvOS(.v12)
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
 	],
 	products: [
 		.library(
