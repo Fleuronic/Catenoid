2a684525edac0fd024036d4caf2ff8bb5f00c4ee
Jordan Kay <jordan@fleuronic.com>
2024-07-08 21:27:00 -0400

    Update fetch

--- Sources/Catenoid/Database.swift
+++ Sources/Catenoid/Database.swift
@@ -3,6 +3,7 @@
 import PersistDB
 
 import struct Catena.IDFields
+import struct Schemata.Projection
 import protocol Catena.Fields
 import protocol Schemata.AnyModel
 
@@ -31,13 +32,25 @@ public extension Database<Store<ReadWrite>> {
 	}
 
 	func fetch<Fields: Catenoid.Fields>(where predicate: Predicate<Fields.Model>? = nil) async -> Result<[Fields]> {
-		let query = predicate.map { Query().filter($0) } ?? Fields.Model.all
-		return await .success(store.fetch(query).value.values)
+		let query = Query(
+			predicates: predicate.map { [$0] } ?? [],
+			order: [],
+			groupedBy: .init(.init(Fields.Model.idKeyPath))
+		)
+		
+		let resultSet: ResultSet<Fields.Model.ID, Fields> = await store.fetch(query).value
+		let values = resultSet.groups.map { group in
+			let values = group.values
+			let fields = values.first!
+			return values.count > 1 ? values.dropFirst().reduce(fields, Fields.merge) : fields
+		}
+
+		return .success(values)
 	}
 
 	func delete<Model: Catenoid.Model>(_ type: Model.Type, where predicate: Predicate<Model.IdentifiedModel>? = nil) async -> Result<[Model.ID]> where Model.ID == Model.IdentifiedModel.ID {
 		await store.delete(.init(predicate)).complete()
 		let fields: Result<[IDFields<Model.IdentifiedModel>]> = await fetch(where: predicate)
-		return fields.map { $0.map(\.id) }
+		return await fields.map { $0.map(\.id) }
 	}
 }
