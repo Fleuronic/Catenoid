3d4b73fe7cf10162cd9d4898f01b8e05dfe631ad
Jordan Kay <jordan@fleuronic.com>
2024-07-10 21:52:38 -0400

    Update storage

--- Sources/Catenoid/Sequence.swift
+++ Sources/Catenoid/Sequence.swift
@@ -1,12 +1,24 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 public extension Sequence {
-	func map<NewElement>(_ transform: (Element) async throws -> NewElement) async rethrows -> [NewElement] {
+	func asyncMap<NewElement>(_ transform: (Element) async throws -> NewElement) async rethrows -> [NewElement] {
 		var values: [NewElement] = []
 		for element in self {
 			try await values.append(transform(element))
 		}
 
 		return values
+	}
+
+	func concurrentMap<NewElement>(_ transform: @escaping (Element) async throws -> NewElement) async rethrows -> [NewElement] {
+		let tasks = map { element in
+			Task {
+				try await transform(element)
+			}
+		}
+
+		return try await tasks.asyncMap { task in
+			try await task.value
+		}
 	}
 }
--- Sources/Catenoid/Storage.swift
+++ Sources/Catenoid/Storage.swift
@@ -7,14 +7,13 @@ public protocol Storage {
 	associatedtype StorageError: Error
 
 	func insert<Model: Catenoid.Model>(_ model: Model) async -> Result<Model.ID, StorageError> where Model.ID == Model.IdentifiedModel.ID
-	func insert<Model: Catenoid.Model>(_ models: [Model]) async -> Result<[Model.ID], StorageError> where Model.ID == Model.IdentifiedModel.ID
 	func fetch<Fields: Catenoid.Fields>(where predicate: Predicate<Fields.Model>?) async -> Result<[Fields], StorageError>
 	func delete<Model: Catenoid.Model>(_ type: Model.Type, where predicate: Predicate<Model.IdentifiedModel>?) async -> Result<[Model.ID], StorageError>  where Model.ID == Model.IdentifiedModel.ID
 }
 
 public extension Storage {
 	func insert<Model: Catenoid.Model>(_ models: [Model]) async -> Result<[Model.ID], StorageError> where Model.ID == Model.IdentifiedModel.ID, StorageError == Never {
-		await .success(models.map(insert).map(\.value))
+		await .success(models.concurrentMap(insert).map(\.value))
 	}
 
 	func delete<Model: Catenoid.Model>(_ type: Model.Type, with ids: [Model.ID]) async -> Result<[Model.ID], StorageError> where Model.ID == Model.IdentifiedModel.ID, StorageError == Never {
