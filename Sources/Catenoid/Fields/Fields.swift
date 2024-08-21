a55c27dc767d69e366e3199ebc21e90a853edb77
Jordan Kay <jordan@fleuronic.com>
2024-08-21 18:37:18 -0400

    Update fields

--- Sources/Catenoid/Fields/Fields.swift
+++ Sources/Catenoid/Fields/Fields.swift
@@ -1,8 +1,15 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+import Schemata
+
 import protocol Catena.Fields
 import protocol PersistDB.ModelProjection
 
-public protocol Fields: Catena.Fields, ModelProjection {
+public protocol Fields<Model>: Catena.Fields, ModelProjection where Model == Self.Model {
 	static func merge(lhs: Self, rhs: Self) -> Self
+}
+
+// MARK: -
+public extension Fields {
+	static func merge(lhs: Self, rhs: Self) -> Self { lhs }
 }
