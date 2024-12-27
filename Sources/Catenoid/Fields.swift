9e2a934fc4c50d79a8140eb4a862d1be6457a373
Jordan Kay <jordan@fleuronic.com>
2024-12-27 10:45:20 -0500

    Remove default implementation

--- Sources/Catenoid/Fields.swift
+++ Sources/Catenoid/Fields.swift
@@ -7,8 +7,3 @@ import protocol PersistDB.ModelProjection
 public protocol Fields<Model>: Catena.Fields, ModelProjection where Model == Self.Model {
 	static func merge(lhs: Self, rhs: Self) -> Self
 }
-
-// MARK: -
-public extension Fields {
-	static func merge(lhs: Self, rhs: Self) -> Self { lhs }
-}
