ecd29c07a429c092698ca09f1f6096198e73bed4
Jordan Kay <jordan@fleuronic.com>
2024-12-27 10:45:32 -0500

    Add default implementation

--- Sources/Catenoid/Architectures/SQLite/Row.swift
+++ Sources/Catenoid/Architectures/SQLite/Row.swift
@@ -5,3 +5,8 @@ import protocol Catena.Representable
 import protocol Identity.Identifiable
 
 public protocol Row: Fields, Representable {}
+
+// MARK: -
+public extension Row {
+	static func merge(lhs: Self, rhs: Self) -> Self { lhs }
+}
