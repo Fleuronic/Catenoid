e299b699140c6bf14774af19fb66ba83008c88b4
Jordan Kay <jordan@fleuronic.com>
2025-07-03 09:45:10 -0400

    Add default merge implementation

--- Sources/Catenoid/Fields.swift
+++ Sources/Catenoid/Fields.swift
@@ -7,3 +7,8 @@ import protocol PersistDB.ModelProjection
 public protocol Fields<Model>: Catena.Fields, ModelProjection where Model == Self.Model {
 	static func merge(lhs: Self, rhs: Self) -> Self
 }
+
+// MARK: -
+extension Fields {
+	public static func merge(lhs: Self, rhs: Self) -> Self { lhs }
+}
